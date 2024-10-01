8b6781e353d144ea6fe076f4bcf055a24f4d1a39
abhi-yo <akshatm.com@gmail.com>
2024-10-01 15:35:51 +0530

    feat: add user update and delete route

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -4,7 +4,7 @@ const configSchema = z.object({
   PORT: z.string().default("3000").transform(Number),
   DATABASE_URL: z
     .string()
-    .default("postgresql://neondb_owner:gXqIh6FlwR2y@ep-late-lake-a58h44vs-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"),
+    .default("postgres://postgres:postgres@localhost:5432/lockout"),
   JWT_SECRET: z.string().default("secret"),
 });
 
